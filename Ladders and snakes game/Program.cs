fd0194ea374ecc860809c4a150a2b26a6fb4db8a
koby <kobyisr1@gmail.com>
2025-11-06 23:51:03 +0200

    Add project files.

---
+++ Ladders and snakes game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class Program
+    {
+        private UserInterface game = new UserInterface();
+        game.StratGame();
+    }
+}
