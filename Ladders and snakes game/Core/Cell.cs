eaf4191cff55c5b752a712ba7750e09f30d8d0e7
koby <kobyisr1@gmail.com>
2025-11-09 18:29:20 +0200

    create cell class

---
+++ Ladders and snakes game/Core/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game.Core
+{
+    internal abstract class Cell
+    {
+        protected int _index;
+        protected Cell(int index)
+        {
+            this._index = index;
+        }
+
+        public abstract int GetIndex();
+    }
+}
