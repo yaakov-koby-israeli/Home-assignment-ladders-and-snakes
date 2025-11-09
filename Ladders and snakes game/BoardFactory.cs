d927acf19f8b3c584cae41d21f8815037c692c4f
koby <kobyisr1@gmail.com>
2025-11-09 12:41:34 +0200

    Create BoardFactory class

---
+++ Ladders and snakes game/BoardFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class BoardFactory
+    {
+
+        public void InitSnakes()
+        {
+
+        }
+    }
+}
