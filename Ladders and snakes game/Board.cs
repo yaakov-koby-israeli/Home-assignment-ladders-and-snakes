cb8b5206d41cb39f5f5a572147b31892af882acc
koby <kobyisr1@gmail.com>
2025-11-09 12:31:56 +0200

    Create class board

---
+++ Ladders and snakes game/Board.cs
@@ -0,0 +1,28 @@
+﻿-using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game
+{
+    internal class Board
+    {
+        private Cell[] _cells;
+
+        private int _boardSize = 0;
+
+        public Board(int boardSize)
+        {
+            SetBoardSize(boardSize);
+            _cells = new Cell[boardSize + 1];
+        }
+
+        // create setBoardSize method for future validation if needed
+        private void SetBoardSize(int boardSize)
+        {
+            _boardSize = boardSize;
+        }
+
+    }
+}
