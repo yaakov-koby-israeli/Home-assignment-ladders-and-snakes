ddfd6e10fe946fbd16e4725e4944d199daa0d216
koby <kobyisr1@gmail.com>
2025-11-09 13:04:24 +0200

    upgrade using RandomProvider singelton

--- Ladders and snakes game/Dice.cs
+++ Ladders and snakes game/Dice.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ladders_and_snakes_game.Utilities;
 
 namespace Ladders_and_snakes_game
 {
     internal class Dice
     {
-        private Random random = new Random();
         public int RollTheDice()
         {
             // real return value between 1 and 6 
-            return random.Next(1, 7); 
+            return RandomProvider.Instance.Next(1, 7); 
         }
     }
 }
