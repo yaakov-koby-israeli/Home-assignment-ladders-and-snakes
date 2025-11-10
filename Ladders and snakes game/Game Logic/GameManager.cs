8d48e6a8ef51a54b0cb4b295a121eb64c57ce6e1
koby <kobyisr1@gmail.com>
2025-11-10 10:03:35 +0200

    create game manager class

---
+++ Ladders and snakes game/Game Logic/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game.Game_Logic
+{
+    internal class GameManager
+    {
+    }
+}
