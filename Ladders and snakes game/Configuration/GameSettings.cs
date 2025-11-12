e4712e2b7fa2e17b2071ed5d2a0d9a01b985408d
koby <kobyisr1@gmail.com>
2025-11-12 15:36:03 +0200

    Add MaxGold propery

--- Ladders and snakes game/Configuration/GameSettings.cs
+++ Ladders and snakes game/Configuration/GameSettings.cs
@@ -11,12 +11,11 @@ namespace Ladders_and_snakes_game.Configuration
         // Default values (you can change them at runtime before starting the game)
         public static int MaxSnakes { get;} = 12;
         public static int MaxLadders { get; } = 12;
+        public static int MaxGold { get; } = 2;
         public static int Rows { get; set; } = 10;
         public static int Cols { get; set; } = 10;
-
         public static int Snakes { get; set; } = 10;
         public static int Ladders { get; set; } = 10;
-
         public static int Players { get; set; } = 2;
 
     }
