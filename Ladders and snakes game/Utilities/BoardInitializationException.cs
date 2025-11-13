1be73d5cbc398b380cfa06afe0ee70762f4494f5
koby <kobyisr1@gmail.com>
2025-11-13 17:58:59 +0200

    Add board Initialization Exception class

---
+++ Ladders and snakes game/Utilities/BoardInitializationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladders_and_snakes_game.Utilities
+{
+    internal sealed class BoardInitializationException : Exception
+    {
+        public BoardInitializationException(string message)
+            : base(message) { }
+
+        public BoardInitializationException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
