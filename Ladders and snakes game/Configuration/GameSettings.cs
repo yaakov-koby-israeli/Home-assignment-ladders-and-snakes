c9110fbbabba6de49483ba7599ee91a5a0069a1c
koby <kobyisr1@gmail.com>
2025-11-13 11:09:24 +0200

    change comment

--- Ladders and snakes game/Configuration/GameSettings.cs
+++ Ladders and snakes game/Configuration/GameSettings.cs
@@ -8,9 +8,9 @@ namespace Ladders_and_snakes_game.Configuration
 {
     internal static class GameSettings
     {
-        // Default values (you can change them at runtime before starting the game)
+        // Default values 
         public static int MaxSnakes { get;} = 12;
-        public static int MaxLadders { get; } = 12;
+        public static int MaxLadders { get; } = 12; 
         public static int MaxGold { get; } = 2;
         public static int Rows { get; set; } = 10;
         public static int Cols { get; set; } = 10;
