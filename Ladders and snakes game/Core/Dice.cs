7a0344ccffa3250cb21321ee7e20868d76407fef
koby <kobyisr1@gmail.com>
2025-11-18 11:12:56 +0200

    Remove dice to core library

---
+++ Ladders and snakes game/Core/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladders_and_snakes_game.Utilities;
+
+namespace Ladders_and_snakes_game.Core
+{
+    internal sealed class Dice
+    {
+        public Dice() { }
+        public int RollTheDice()
+        {
+            // real return value between 1 and 6 
+            return RandomProvider.Instance.Next(1, 7); 
+        }
+    }
+}
