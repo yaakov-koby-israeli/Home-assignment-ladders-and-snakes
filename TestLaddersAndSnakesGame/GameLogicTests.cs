6beb11de3dc3e63bbcae57762f87a76fa2d05d05
koby <kobyisr1@gmail.com>
2025-11-19 13:34:39 +0200

    create game  manager class Add role dice unit tests

---
+++ TestLaddersAndSnakesGame/GameLogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Ladders_and_snakes_game.Game_Logic;
+using Xunit;
+
+namespace TestLaddersAndSnakesGame
+{
+    public class GameLogicTests
+    {
+        [Fact]
+        public void RollDices_ResultIsBetween2And12()
+        {
+            GameManager gameManager = new GameManager();
+            int res = gameManager.RollDices();
+
+            Assert.InRange(res, 2, 12);
+        }
+
+        [Fact]
+        public void RollDicesLoopCheck()
+        {
+            GameManager gameManager = new GameManager();
+
+            for (int  i=0 ; i < 100 ;i++)
+            {
+                int res = gameManager.RollDices();
+
+                Assert.InRange(res, 2, 12);
+            }
+        }
+    }
+}
